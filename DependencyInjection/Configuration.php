482e1af3868609f0bbc1c3f1c2343eadeb7dd647
imptum <imptum@gmail.com>
2014-11-11 18:48:54 +0700

    Remove unused code from configuration
    - Remove flatNode function
    - Remove beforeNormalization

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,22 +23,7 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('zetta_menu');
 
-        $rootNode
-            // ->beforeNormalization()
-            //     ->always(function($config)
-            //     {
-            //         if(isset($config['menus']))
-            //         {
-            //             foreach($config['menus'] as $idx => $node)
-            //             {
-            //                 $config['menus'][$idx] = $this->flatNode($node);
-            //             }
-            //         }
-            //         return $config;
-            //     })
-            // ->end()
-            ->fixXmlConfig('menu', 'menus')
-        ;
+        $rootNode->fixXmlConfig('menu', 'menus');
         $this->buildMenuNode(
             $rootNode
             ->children()
@@ -54,44 +39,6 @@ class Configuration implements ConfigurationInterface
         ;
 
         return $treeBuilder;
-    }
-
-    /**
-     * Flat a node
-     * @param array node
-     */
-    private function flatNode(array $node, $parent = null)
-    {
-        foreach($node as $idx => $item)
-        {
-            if(!is_array($item))
-            {
-                $msg = sprintf('Invalid node "%s" under "%s"', $idx, $parent);
-                $ex = new InvalidConfigurationException($msg);
-                $ex->setPath($parent);
-                throw $ex;
-            }
-            $key = $parent?"${parent}.${idx}":$idx;
-            $node[ $key ] = $item;
-            if($key != $idx ) unset($node[$idx]);
-            $node[ $key ]['parent'] = $parent;
-            $node[ $key ]['uri'] = isset($node[ $key ]['uri'])?$node[ $key ]['uri']:null;
-            $node[ $key ]['route'] = isset($node[ $key ]['route'])?$node[ $key ]['route']:null;
-            $node[ $key ]['routeParameters'] = isset($node[ $key ]['routeParameters'])?$node[ $key ]['routeParameters']:null;
-            if(array_key_exists('children', $item))
-            {
-                if(!is_array($item['children']))
-                {
-                    $msg = sprintf('Invalid node "%s" under "%s"', 'children', $key);
-                    $ex = new InvalidConfigurationException($msg);
-                    $ex->setPath($parent);
-                    throw $ex;
-                }
-                $node = array_merge($node, $this->flatNode($item['children'], $key) );
-                unset($node[$key]['children']);
-            }
-        }
-        return $node;
     }
 
     protected function buildMenuNode(NodeDefinition $node)
